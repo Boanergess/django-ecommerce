41b54628bed7e8401126274d510f11dc7db3e9fe
Samuel <samueliseo99@gmail.com>
2024-06-19 00:00:25 -0400

    jazzmin e imagenes

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
 class MyAccountManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
@@ -11,27 +10,23 @@ class MyAccountManager(BaseUserManager):
             raise ValueError('El usuario debe tener un username')
         
         user = self.model(
-            email = self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
         )
-
         user.set_password(password)
         user.save(using=self._db)
-
-        
         return user
     
     def create_superuser(self, first_name, last_name, email, username, password):
         user = self.create_user(
-            email = self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
         )
-
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
@@ -39,14 +34,14 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-class Account(AbstractBaseUser):
+class Account(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=50)
 
-    #campos atributos de django
+    # campos atributos de django
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
@@ -70,11 +65,11 @@ class Account(AbstractBaseUser):
     
     def has_module_perms(self, add_label):
         return True
-    
+
 class UserProfile(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
-    address_line_1 =  models.CharField(blank=True, max_length=100)
-    address_line_2 =  models.CharField(blank=True, max_length=100)
+    address_line_1 = models.CharField(blank=True, max_length=100)
+    address_line_2 = models.CharField(blank=True, max_length=100)
     profile_picture = models.ImageField(blank=True, upload_to='userprofile')
     city = models.CharField(blank=True, max_length=20)
     state = models.CharField(blank=True, max_length=20)
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
-    'jazzmin'
 ]
 
 MIDDLEWARE = [
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'ecommerce/static'
